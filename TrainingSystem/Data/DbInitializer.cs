4769ed8335956c37236f12a4cbbfbb7e703be763
wander <anhkhoa24082001@gmail.com>
2022-11-01 19:47:08 +0700

    init

---
+++ TrainingSystem/Data/DbInitializer.cs
@@ -0,0 +1,54 @@
+﻿using BookManager.Entity;
+using System.Linq;
+
+namespace BookManager.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ModelContext context)
+        {
+            context.Database.EnsureCreated();
+            // Look for any students.
+            if (context.Author.Any())
+            {
+                return; // DB has been seeded
+            }
+            var books = new Book[]
+            {
+                new Book{
+                    Title="Nam Ngu",
+                    Desciption="Tieng Nam Ngu hay duoc su dung",
+                    Date= new System.DateTime(2001,12,30),
+                    Rent=false
+                },
+                new Book{
+                    Title="Hehe",
+                    Desciption="....",
+                    Date= new System.DateTime(2002,08,30),
+                    Rent=false
+                }
+            };
+
+            var authors = new Author[]
+            {
+                new Author{ 
+                    Name="Wander",
+                    Age= 21
+                },
+                new Author{
+                    Name="Kygor",
+                    Age= 21
+                }
+            };
+
+            authors[0].Book.Add(books[0]);
+            authors[0].Book.Add(books[1]);
+
+            foreach (Author author in authors)
+            {
+                context.Author.Add(author);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ TrainingSystem/Data/ModelContext.cs
@@ -0,0 +1,19 @@
+﻿using BookManager.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManager.Data
+{
+    public partial class ModelContext: DbContext{
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options){}
+
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Author> Author { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("Book");
+            modelBuilder.Entity<Author>().ToTable("Author");
+        }
+    }
+}
