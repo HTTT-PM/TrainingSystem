b1a83e3eea7adccfdcd372ae044a6b8556f19b56
Điền Tạ <63856613+blueackerman06@users.noreply.github.com>
2022-11-11 13:40:08 +0700

    Create api create training program
    APi tạo chương trình training

--- TraniningSystemAPI/Data/ModelContext.cs
+++ TraniningSystemAPI/Data/ModelContext.cs
@@ -3,8 +3,9 @@ using TraniningSystemAPI.Entity;
 
 namespace TraniningSystemAPI.Data
 {
-    public partial class ModelContext: DbContext{
-        public ModelContext(DbContextOptions<ModelContext> options) : base(options){}
+    public partial class ModelContext : DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options) { }
 
         public DbSet<Trainer> Trainer { get; set; }
         public DbSet<Classroom> Classroom { get; set; }
@@ -12,15 +13,22 @@ namespace TraniningSystemAPI.Data
         public DbSet<Exercise> Exercise { get; set; }
         public DbSet<Course> Course { get; set; }
         public DbSet<ClassroomDetail> ClassroomDetail { get; set; }
+        public DbSet<TrainingProgram> TrainingProgram { get; set; }
+        public DbSet<Knowledge> Knowledge { get; set; }
+        public DbSet<Skill> Skill { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Knowledge>().ToTable("Knowledge");
+            modelBuilder.Entity<TrainingProgram>().ToTable("TrainingProgram");
+            modelBuilder.Entity<Skill>().ToTable("Skill");
             modelBuilder.Entity<Trainer>().ToTable("Trainer");
             modelBuilder.Entity<Classroom>().ToTable("Classroom");
             modelBuilder.Entity<Document>().ToTable("Document");
             modelBuilder.Entity<Exercise>().ToTable("Exercise");
             modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<ClassroomDetail>().HasKey(x=> new { x.ClassroomKey, x.CourseKey });
+            modelBuilder.Entity<ClassroomDetail>().HasKey(x => new { x.ClassroomKey, x.CourseKey });
             modelBuilder.Entity<ClassroomDetail>()
                 .HasOne(t => t.Classroom)
                 .WithMany(t => t.ClassroomDetails)
@@ -34,3 +42,4 @@ namespace TraniningSystemAPI.Data
         }
     }
 }
+
---
+++ TraniningSystemAPI/Entity/TrainingProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TraniningSystemAPI.Entity
+{
+    public partial class TrainingProgram
+    {
+        public TrainingProgram()
+        {
+            Knowledge = new HashSet<Knowledge>();
+            Skill = new HashSet<Skill>();
+        }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int TrainingID { get; set; }
+        public string TrainingName { get; set; }
+        public virtual ICollection<Knowledge> Knowledge { get; set; }
+        public virtual ICollection<Skill> Skill { get; set; }
+
+    }
+}
