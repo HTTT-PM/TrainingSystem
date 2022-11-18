a5d2d744ca0e8327d2638dabd1b922b0fa751488
Điền Tạ <63856613+blueackerman06@users.noreply.github.com>
2022-11-18 11:26:47 +0700

    Update Classroom.cs

--- TraniningSystemAPI/Entity/Classroom.cs
+++ TraniningSystemAPI/Entity/Classroom.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace TraniningSystemAPI.Entity
 {
@@ -9,6 +10,7 @@ namespace TraniningSystemAPI.Entity
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
+        [JsonIgnore]
         public int ClassroomID { get; set; }
         public string ClassroomName { get; set; }
         public string Desciption { get; set; }
